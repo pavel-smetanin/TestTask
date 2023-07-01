387a51a7a49b137d4822acd2a8058f2275ce0c9f
pavel-smetanin <pavel.smetanin.01@mail.ru>
2023-07-01 17:36:57 +0300

    Задание 1

---
+++ ShtafunTestTask/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ShtafunTestTask
+{
+    class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                Row row = new Row();
+                //Здесь можно ввести путь к изображениям
+                string img1 = "../../../Images/1.jpg";
+                string img2 = "../../../Images/2.jpg";
+                string img3 = "../../../Images/3.jpg";
+                string img4 = "../../../Images/4.jpg";
+                string resultImg = "../../../Images/test.png";
+                row.ImageAdd(img1);
+                row.ImageAdd(img2);
+                row.ImageAdd(img3);
+                row.ImageAdd(img4);
+                Console.Write("Input the width of result image: ");
+                string width = Console.ReadLine();
+                var result = row.GetStoryboardImage(Int32.Parse(width));
+                result.Save(resultImg);
+                Console.WriteLine($"The result image is saved in: {resultImg}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
+
+
---
+++ ShtafunTestTask/Row.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+
+
+namespace ShtafunTestTask
+{
+    internal class Row
+    {
+        public List<Bitmap> Images { get; private set; } 
+        public Row()
+        {
+            Images = new List<Bitmap>();
+        }
+        public void ImageAdd(string filename)
+        {
+            Bitmap image = (Bitmap)Bitmap.FromFile(filename);
+            Images.Add(image);
+        }
+
+        public Bitmap GetStoryboardImage(int widthImage)
+        {
+            //Переменная для поиска минимума
+            int heightImage = Images[0].Height; 
+            //Переменная сумматор
+            int sumWidth = 0;
+            //Нахождение суммы ширин и поиск минимальной высоты изображений
+            //Высота результирующего изображения будет равна минимальной высоте 
+            foreach (var i in Images)
+            {
+                sumWidth += i.Width;
+                if (i.Height < heightImage)
+                    heightImage = i.Height;
+            }
+            //Коэффициент увеличения(уменьшения) длин с учетом введенной результирующей ширины
+            float delta = widthImage / (float)sumWidth;
+            heightImage = (int)Math.Ceiling(heightImage * delta);
+            //Результирующее изображение
+            Bitmap bitmap = new Bitmap(widthImage, heightImage);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            int localWidth = 0;
+            //Перебор всех изображений
+            foreach (var i in Images)
+            {
+                //Коэффициент пропорциональности изображения
+                float propCoef = (float)i.Height / i.Width;
+                //Новая ширина
+                int newWidth = (int)Math.Ceiling(heightImage / propCoef);
+                //Размещения измененного изображения в результирующем
+                graphics.DrawImage(i, localWidth, 0, newWidth, heightImage);
+                localWidth += newWidth;
+            }
+            return bitmap;
+        }
+
+    }
+}
